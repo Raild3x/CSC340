f897e841240400d6f082090396fc3d7643f35b0c
Raild3x <47638612+Raild3x@users.noreply.github.com>
2020-10-07 16:53:19 -0400

    Update Signal.java

--- src/objects/Signal.java
+++ src/objects/Signal.java
@@ -1,5 +1,14 @@
-package objects;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Objects;
 
+/**
+ *
+ * @author Logan
+ */
 import java.util.HashSet;
 import java.util.function.Consumer;
 
